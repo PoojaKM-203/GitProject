994c7d41d80956675894b34142394d732bf8b8e4
PoojaKM-203 <poojakmsn@gmail.com>
2025-02-19 13:52:48 +0530

    c file is added

---
+++ c.java
@@ -0,0 +1,10 @@
+
+public class c {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("First Program");
+        int a = 10;
+        System.out.println(a);
+    }
+}
